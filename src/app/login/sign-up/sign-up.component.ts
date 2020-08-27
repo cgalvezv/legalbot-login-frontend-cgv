10e33f845386f51a2829e4e73db3cb91f23a0f62
cgalvezv <camilogalvezv@gmail.com>
2020-08-27 00:22:45 -0400

    feature(sign_up): change behavior to show validation msgs

--- src/app/login/sign-up/sign-up.component.ts
+++ src/app/login/sign-up/sign-up.component.ts
@@ -17,6 +17,31 @@ export class SignUpComponent implements OnInit {
     password: new FormControl('', [Validators.required, Validators.maxLength(8) ,Validators.pattern(/[*$@!#%&()^~{}]+/)]), // Password validation
   });
 
+  validationMessages = {
+    global: {
+      required: 'Required',
+    },
+    name: {
+      hint: 'At least 3 characters long',
+      errors: {
+        minLength: 'Wrong password length (Min length: 3)'
+      }
+    },
+    email: {
+      hint: 'Should be a valid email',
+      errors: {
+        email: 'Invalid email format'
+      }
+    },
+    password: {
+      hint: 'Should contain at least one special character (ie: *$@!#%&()^~{}) and be at least 8 characters long',
+      errors: {
+        pattern: 'Invalid format, password must contain least one special character (*$@!#%&()^~{})',
+        maxLength: 'Wrong password length (Max length: 8)'
+      }
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
